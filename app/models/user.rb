8041ecfc93483a2813bec9a1e0f18b60477c087b
Alexandre Guimarães <alexandre404@gmail.com>
2015-09-03 06:23:11 +0000

    Stripe Server-Side Code

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  
+  attr_accessor :stripe_card_token #pega o valor de campo hidden do form.
+  
+  def save_with_payment
+    if valid? #verifica a validação do formulário.
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id # id retornado pelo stripe. A coluna stripe_customer_token ainda será criada mais adiante no banco de dados
+      save! # Salva no banco de dados, mas falta criar uma coluna.
+    end
+  end
+
 end
